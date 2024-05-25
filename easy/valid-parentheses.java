b67aa2610422bb356006c568e181482e2ca2b0f0
Esteban Perafan <bestebanperafan@gmail.com>
2024-05-25 14:12:06 -0400

    Update valid-parentheses solution

--- easy/valid-parentheses.java
+++ easy/valid-parentheses.java
@@ -1,5 +1,45 @@
 class Solution {
+    // SOLUCIÓN 2 - Implementando Stack nativo de Java
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        // Si solo hay un parentesis o ninguno entonces no es valido
+        if(s.length() < 2){
+            return false;
+        }
+        
+        // Recorrer cada parentesis
+        for(int i = 0; i < s.length(); i++){
+            // Agregar a la pila los parentesis de cierre correspondiente de cada uno
+            switch(s.charAt(i)){
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    // Si la pila no esta vacia (tiene parentesis de cierre) y el elemento en la cima es el cierre del ultimo parentesis abierto
+                    if(!stack.isEmpty() && s.charAt(i) == stack.peek()){
+                        // Cerrar parentesis borrandolo
+                        stack.pop();
+                    } else {
+                        // Si la pila estaba vacia y el elemento no coincidia con la cima entonces o es un elemento de cierre de parentesis sin previa apertura o es un elemento de cierre de parentesis para el parentesis equivocado
+                        return false;
+                    }
+            }
+        }
+
+        // Si todos los parentesis se cerraron adecuadamente entonces la pila estara vacia y sera valido
+        return stack.isEmpty();
+    }
+
+    /**
+        SOLUCIÓN 1
+        public boolean isValid(String s) {
         StackP st = new StackP(s.length());
 
         if(s.length() == 1){
@@ -65,4 +105,5 @@ class StackP {
             return;
         }
     }
+    */
 }
